38056ba188aff499a20b1a43d97551667f66bdd9
hhbb0081 <likesun2000@naver.com>
2024-10-11 17:29:39 +0900

    Feat: 241011

--- nextvite/lib/auth.ts
+++ nextvite/lib/auth.ts
@@ -1,5 +1,6 @@
 // lib/auth.ts for google
 import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
 import Google from 'next-auth/providers/google';
 
 export const {
@@ -8,5 +9,43 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
-  providers: [Google],
+  providers: [
+    Credentials({
+      name: 'Email',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'example@example.com',
+        },
+        passwd: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials || !credentials.email || !credentials.passwd)
+          return null;
+
+        console.log('🚀  credentials:', credentials);
+        return null;
+      },
+    }),
+    Google,
+  ],
+
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const didLogin = !!auth?.user;
+      console.log(
+        '🚀 auth.ts > callbacks > authorized - didLogin:',
+        didLogin,
+        nextUrl.pathname
+      );
+
+      if (nextUrl.pathname.startsWith('/about')) {
+        return didLogin;
+      }
+      if (didLogin) return Response.redirect(new URL('/about', nextUrl));
+
+      return true;
+    },
+  },
 });
